efbbca3ed099e0cbbef0f5ff63ff8508f7260edc
Ian Okumu <okumu.otsembo@gmail.com>
2022-08-25 13:14:04 +0300

    Incomplete

--- Rakefile
+++ Rakefile
@@ -1,8 +1,32 @@
-require_relative './config/environment'
-require 'sinatra/activerecord/rake'
+# require_relative './config/environment'
+# require 'sinatra/activerecord/rake'
 
 # CREATE TASK WITHOUT DESCRIPTION
+task :hello_world do
+    puts "Hello World"
+end
 
 # CREATE TASK WITH DESCRIPTION
+desc 'Find .01% of 14M'
+task :complex_math do
+    output = (0.01/100) * 14000000
+    puts output
+end
 
 # ORGANIZE TASKS IN NAMESPACES
+namespace :math_concepts do
+    desc 'Find addition'
+    task :add, [:num1, :num2] do
+        puts "Addition: #{num1}, #{num2}"
+    end 
+
+    desc 'Find subtraction'
+    task :sub do
+        puts "Subtraction"
+    end
+
+    desc 'Find division'
+    task :div do
+        puts "Division"
+    end
+end
--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,49 @@ require "pry"
 require "active_record"
 
 # TODO: CONNECT TO THE DATABASE
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/school.db"
+)
 
 # TODO: CREATE TABLE
+create_courses_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS courses (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    duration INTEGER )
+SQL
+
+create_grades = <<-SQL
+  CREATE TABLE IF NOT EXISTS grades (
+    id INTEGER PRIMARY KEY,
+    score INTEGER NOT NULL,
+    UNIQUE(score)
+  )
+SQL
+
+create_scores = <<-SQL
+  CREATE TABLE IF NOT EXISTS scores (
+    id INTEGER PRIMARY KEY,
+    score INTEGER NOT NULL,
+    UNIQUE(score)
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(create_courses_table)
+ActiveRecord::Base.connection.execute(create_grades)
+ActiveRecord::Base.connection.execute(create_scores)
 
 # TODO: Create class that associates with the db table
+class Course < ActiveRecord::Base
+end
+
+class Grade < ActiveRecord::Base
+end
+
+class Score < ActiveRecord::Base
+end
 
 # TODO: VIEW METHODS AVAILABLE FOR TABLE (methods)
 
@@ -20,3 +59,5 @@ require "active_record"
 # TODO: FIND BY GIVEN CONDITIONS (find_by)
 
 # TODO: UPDATE RECORDS (save)
+
+binding.pry
